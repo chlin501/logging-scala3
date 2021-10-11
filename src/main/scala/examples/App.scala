8233b0a0acf969366a623219a35a17923194cf15
chlin501 <clin4j@googlemail.com>
2021-10-12 00:54:17 +0800

    Switch to use implicit prioritization

--- src/main/scala/examples/App.scala
+++ src/main/scala/examples/App.scala
@@ -2,9 +2,23 @@ package examples
 
 import logging._
 
-@main def execute(): Unit =
-  import Logging.given
-  //given instance: Logging[String] = Logging.logger
-  val log = Logging()
-  log.info("hi!")
+object App {
+
+  def runWithPrintln(): Unit = {
+    val log = Logging[String]
+    log.info("hi!")
+  }
+
+  def runWithSlf4jLogger(): Unit = {
+    import org.slf4j._
+    given slf4jLogger: Logger = LoggerFactory.getLogger(classOf[App])
+    val log = Logging[String]
+    log.info("hi!")
+  }
+
+  def main(args: Array[String]): Unit = {
+    runWithPrintln()
+    runWithSlf4jLogger()
+  }
+}
 
--- src/main/scala/logging/Logging.scala
+++ src/main/scala/logging/Logging.scala
@@ -1,24 +1,42 @@
 package logging
 
+import org.slf4j.{LoggerFactory, Logger => Slf4jLogger}
+
 trait Logging[T]:
   def info(f: => T): T
   def debug(f: => T): T
   def warn(f: => T): T
   def error(f: => T): T
 
-object Logging {
-
-  def apply[T: Logging](): Logging[T] = summon[Logging[T]]
+trait LowPriority {
 
   given consoLogger: Logging[String] = new Logging[String] {
 
     def info(f: => String): String = { println(f); f }
-  
+
     def debug(f: => String): String = { println(f); f }
 
     def warn(f: => String): String = { println(f); f }
 
     def error(f: => String): String = { println(f); f }
+
+  }
+
+}
+
+object Logging extends LowPriority {
+
+  def apply[T](using ev: Logging[T]): ev.type = ev
+
+  given slf4jLogger (using log: Slf4jLogger): Logging[String] = new Logging[String] {
+
+    def info(f: => String): String = { log.info(f); f }
+
+    def debug(f: => String): String = { log.debug(f); f }
+
+    def warn(f: => String): String = { log.debug(f); f }
+
+    def error(f: => String): String = { log.debug(f); f }
 
   }
 
